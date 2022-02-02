a5366b36b176f24da63a2c524feea49d76150890
PcDiNiS <dinis.paulo@gmail.com>
2022-02-02 17:16:01 +0000

    minor changes

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -7,35 +7,20 @@ sap.ui.define([
 	return Controller.extend("pt.pcdinis.ui5webcenter.webcenter.controller.MainView", {
 
 		onInit: function(evt) {
-			var tile1 = this.getView().byId("tile1");
-			var tileZOrangeGetLeave = this.getView().byId("idTileZOrangeGetLeave");
-			tile1.setVisible(false);
-			tileZOrangeGetLeave.setVisible(false);
-			// switch (userLevel) {
-			// 	// User
-			// 	case "1":
-			// 		tile1.setVisible(true);
-			// 		break;
-			// 		// Admin				
-			// 	case "2":
-			// 		tile1.setVisible(true);
-			// 		tileZOrangeGetLeave.setVisible(true);
-			// 		break;
-			// }
-					tile1.setVisible(true);
-					tileZOrangeGetLeave.setVisible(true);			
+		
 		},
 
 		handleLogOffPress: function(evt) {
-			window.location.replace("https://www.deploy.pt/deployApps/users/logout.php");
+			window.location.replace("https://www.pcdinis.com/webcenter/");
 		},
 
-		pressTile1: function(evt) {
-			MessageToast.show("The Tile is pressed.");
+		pressTileMemGame: function(evt) {
+			window.location.replace("https://www.pcdinis.com/memorygame");
+			//MessageToast.show("The Tile is pressed.");
 		},
 
-		pressTileZOrangeGetLeave: function(evt) {
-			window.location.replace("https://www.deploy.pt/deployApps/ZOrangeGetLeave/webapp/index.php");
+		pressTileMastermind: function(evt) {
+			window.location.replace("https://www.pcdinis.com/mastermind");
 		}
 
 	});
